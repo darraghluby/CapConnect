2481ea52b20ba850837becfa2a41f444abbb56f2
Darragh <darraghluby@gmail.com>
2023-11-25 16:01:06 +0000

    ????????

--- Hello.py
+++ Hello.py
@@ -10,27 +10,22 @@ website_icon = Image.open("images/website_home_page_icon.png")
 st.set_page_config(
     page_title="CapConnect",
     page_icon=website_icon,
-initial_sidebar_state="collapsed"
+    initial_sidebar_state="collapsed"
 )
-
-st.image(website_logo)
 
 login_attempts = 0
 
-
-
+st.image(website_logo)
 st.markdown(
     """
-<style>
-    [data-testid="collapsedControl"] {
-        display: none
-    }
-</style>
-""",
-    unsafe_allow_html=True,
+    <style>
+        [data-testid="collapsedControl"] {
+            display: none
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
-    )
-
 
 home_page_md = """
 
@@ -60,8 +55,6 @@ Our goal is to enhance the overall experience for everyone involved in the cance
 """
 
 
-
-
 def user_entry_page(name):
     st.write("# Welcome to CapConnect! 👋")
     st.write(f'### **Welcome *{name}***')
@@ -73,7 +66,6 @@ def user_entry_page(name):
     """)
     st.markdown(home_page_md)
     authenticator.logout('Logout', 'main')
-
 
 
 def med_staff_entry_page(name):
@@ -101,6 +93,7 @@ authenticator = Authenticate(
     config['preauthorized'],
 )
 
+
 def showBar():
     st.markdown(
         """
@@ -112,6 +105,7 @@ def showBar():
     """,
         unsafe_allow_html=True,
     )
+
 
 name, authentication_status, username = authenticator.login('Login', 'main')
 
@@ -126,16 +120,10 @@ if authentication_status:
         med_staff_entry_page(name)
         showBar()
 
-
-
 elif not authentication_status:
     if login_attempts > 0:
         st.error('Username/password is incorrect')
     login_attempts += 1
 
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
-
 elif authentication_status is None:
     st.warning('Please enter your username and password')
-
