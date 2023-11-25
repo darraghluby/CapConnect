d2395b49a99e907d7ce30f747e4a9944ffd313f7
DJK101 <danieljeffersonkennedy@gmail.com>
2023-11-25 16:20:39 +0000

    Fix bug with reviews

--- pages/2 Patient Center.py
+++ pages/2 Patient Center.py
@@ -1,10 +1,7 @@
 import base64
-
 import streamlit as st
-import pandas as pd
-import pydeck as pdk
-from urllib.error import URLError
 from streamlit_star_rating import st_star_rating
+
 
 def displayPDF(file):
     # Opening file from file path
@@ -17,6 +14,7 @@ def displayPDF(file):
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
 
+
 st.set_page_config(page_title="Patient Information")
 
 st.markdown("# Patient Feedback and Information")
@@ -25,22 +23,24 @@ st.write(
     """This is the Patient Feedback and Information Center, please choose an option in the sidebar"""
 )
 
-if st.sidebar.button('Give my feedback'):
-    stars = st_star_rating(label="Please rate you experience with <Treatment Name>"
+st.sidebar.button('Feedback')
+ic_button = st.sidebar.button('Information Center')
+
+if ic_button:
+    displayPDF("/home/djk/Downloads/Paxman-Patient-Brochure-US-WEB.pdf")
+else:
+    stars = st_star_rating(label="Please rate you experience with Paxman ColdCaps"
                            , maxValue=10
                            , defaultValue=0, key="rating1",
                            dark_theme=False
                            )
-    stars = st_star_rating(label="Please rate you experience with <Hospital Name>"
+    stars = st_star_rating(label="Please rate you experience with UHL"
                            , maxValue=10
                            , defaultValue=0, key="rating2",
                            dark_theme=False
                            )
-    stars = st_star_rating(label="Please rate you experience with <Consultant Name>"
+    stars = st_star_rating(label="Please rate you experience with Dr. Dre"
                            , maxValue=10
                            , defaultValue=0, key="rating3",
                            dark_theme=False
                            )
-
-if st.sidebar.button('Information Center'):
-    displayPDF("/home/djk/Downloads/Paxman-Patient-Brochure-US-WEB.pdf")
